f022c2f2e1743427b25c4e38a87128eb57680652
Shane <shaneconway1998@gmail.com>
2023-01-10 19:47:19 +0000

    learning how to use arrow functions in js es6

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,38 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(x, y) {
+//     // Code block
+//     return x + y;
+// }
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers = (x, y) => {
+    // Code block
+    return x + y;
+}
+let sum = addTwoNumbers(2, 5);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (x, y) => (x + y); also valid
+const addTwoNumbers2 = (x, y) => x + y;
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething("Hello world!");
+
+const sayHello = () => console.log("Hello there!");
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+        This is a multiline string!
+    </p>
+    `
+)
+
+console.log(returnMultipleLines());
